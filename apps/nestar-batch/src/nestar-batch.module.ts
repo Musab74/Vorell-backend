60bc61ffd94adc541420cfbeeb70ced453b660d9
Musab74 <Rmcbaxr@gmail.com>
2025-06-01 21:39:41 +0900

    fit: Configure environmental variables

--- apps/nestar-batch/src/nestar-batch.module.ts
+++ apps/nestar-batch/src/nestar-batch.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { NestarBatchController } from './nestar-batch.controller';
 import { NestarBatchService } from './nestar-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [NestarBatchController],
+ imports: [
+    ConfigModule.forRoot()
+  ],  controllers: [NestarBatchController],
   providers: [NestarBatchService],
 })
 export class NestarBatchModule {}
