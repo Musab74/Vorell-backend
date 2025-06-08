219e528f4980f38a13e6c35bf915eef39117b0aa
Musab74 <Rmcbaxr@gmail.com>
2025-06-08 19:52:12 +0900

    feat:develop loging via global interceptors

--- apps/nestar-api/src/libs/interceptors/login.Interceptor.ts
+++ apps/nestar-api/src/libs/interceptors/login.Interceptor.ts
@@ -1,18 +1,54 @@
+import {
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+    Logger,
+  } from '@nestjs/common';
+  import {
+    GqlContextType,
+    GqlExecutionContext,
+  } from '@nestjs/graphql';
+  import { Observable } from 'rxjs';
+  import { tap } from 'rxjs/operators';
+  
+  @Injectable()
+  export class LoggingInterceptor implements NestInterceptor {
+    private readonly logger: Logger = new Logger();
+  
+    public intercept(
+      context: ExecutionContext,
+      next: CallHandler
+    ): Observable<any> {
+      const recordTime = Date.now();
+      const requestType = context.getType<GqlContextType>();
+  
+      if (requestType === 'http') {
+        // Develop if needed!
+      } else if (requestType === 'graphql') {
+        // Step 1 Print request
+        const gqlContext = GqlExecutionContext.create(context);
+        this.logger.log(
+          `${this.stringify(gqlContext.getContext().req.body)}`,
+          'Request'
+        );
+         // Step 2 Error handling via GraphQl
 
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-
-@Injectable()
-export class LoggingInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
-
-    const now = Date.now();
-    return next
-      .handle()
-      .pipe(
-        tap(() => console.log(`After... ${Date.now() - now}ms`)),
-      );
+         //Step 3 No errors giving response below
+        return next.handle().pipe(
+          tap((context) => {
+            const responseTime = Date.now() - recordTime;
+            this.logger.log(`${this.stringify(context)} - ${responseTime}ms \n\n`, 'Response');
+          })
+        );
+      }
+  
+      return next.handle(); 
+    }
+  
+    private stringify(data: any): string {
+      console.log(typeof data);
+      return JSON.stringify(data).slice(0, 75);
+    }
   }
-}
+  
