a4185d5eff3a96e180fafe900da7ba37de337603
Musab74 <Rmcbaxr@gmail.com>
2025-06-16 13:11:07 +0900

    feat:modify upload images

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptors/login.Interceptor';
+import {graphqlUploadExpress} from "graphql-upload"
+import * as express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe)
+  app.enableCors({origin:true, credentials:true})
   app.useGlobalInterceptors(new LoggingInterceptor());
+
+  app.use(graphqlUploadExpress({maxSize:15000000, maxFiles:10}))
+  app.use("/upload", express.static("./uploads"))
+
+
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
