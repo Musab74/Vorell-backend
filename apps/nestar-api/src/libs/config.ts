9eeb9e45f199a1fb855972dc9cbc16d898530de8
Musab74 <Rmcbaxr@gmail.com>
2025-06-23 17:26:38 +0900

    feat:develop following followers all APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -17,6 +17,14 @@ export const availableOptions = ['propertyBarter', 'propertyRent'];
 export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
 export const availableCommentSorts = ['createdAt', 'updatedAt'];
 
+
+export const shapeIntoMongoObjectId = (target: any) => {
+  return typeof target === "string" ? new ObjectId(target) : target;
+}
+export const shapeId = (target: any) => {
+  return typeof target === 'string' ? new ObjectId(target) : target;
+};
+
 export const availablePropertySorts = [
   'createdAt',
   'updatedAt',
@@ -35,9 +43,21 @@ export const lookUpMember = {
   },
 };
 
-export const shapeIntoMongoObjectId = (target: any) => {
-  return typeof target === "string" ? new ObjectId(target) : target;
-}
-export const shapeId = (target: any) => {
-  return typeof target === 'string' ? new ObjectId(target) : target;
+export const lookupFollowingData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followingId',
+		foreignField: '_id',
+		as: 'followingData',
+	},
 };
+
+export const lookupFollowerData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followerId',
+		foreignField: '_id',
+		as: 'followerData',
+	},
+};
+
